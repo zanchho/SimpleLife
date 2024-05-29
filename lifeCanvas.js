c7b34b7cdd8629ad4c670f9eae218cdb75ebd4df
zanchho <zanchho391@gmail.com>
2024-05-29 20:08:04 +0200

    removed some comments and used prettier

--- lifeCanvas.js
+++ lifeCanvas.js
@@ -1,4 +1,3 @@
-//vars
 const initTypes = [
   { name: "red", amount: "200", color: "red" },
   { name: "green", amount: "300", color: "green" },
@@ -9,7 +8,6 @@ let isPaused = false
 let PARTICEL_SIZE = 5
 let multiplier = 2
 
-//handle Canvas
 let canvas = document.getElementById("life")
 canvas.width = canvas.getBoundingClientRect().width
 canvas.height = canvas.getBoundingClientRect().height
@@ -21,7 +19,7 @@ const WINDOW = {
 const ctx = canvas.getContext("2d")
 const particles = []
 let rulesArr = []
-//handle Settings
+
 const settingButton = document.getElementById("btn_settings")
 const settingPanel = document.getElementById("settings_panel")
 const panelContent = document.getElementById("panel_content")
@@ -50,7 +48,7 @@ toggleSettingsPanel()
 settingButton.addEventListener("click", toggleSettingsPanel)
 
 const Types = []
-//create Types
+
 const typeName = document.getElementById("type_name")
 const typeAmount = document.getElementById("type_amount")
 const typeColor = document.getElementById("type_color")
@@ -79,7 +77,6 @@ const getTypeEntry = obj => {
   return entry
 }
 const addType = (name, amount, color) => {
-  //TODO do UI prettier
   const obj = { name: name, amount: amount, color: color }
   let li = document.createElement("li")
   li.appendChild(getTypeEntry(obj))
@@ -193,6 +190,7 @@ function rule(p1, p2, g) {
 
     a.x += a.vx
     a.y += a.vy
+
     //dodge on borders
     if (a.x <= 0 || a.x >= WINDOW.width - PARTICEL_SIZE) a.vx *= -1
     if (a.y <= 0 || a.y >= WINDOW.height - PARTICEL_SIZE) a.vy *= -1
@@ -216,7 +214,7 @@ function createRuleSetUI() {
   }
   const rootOfListID = "rules_list"
   const _el = document.getElementById(rootOfListID)
-  _el.innerHTML = "" //reset inner
+  _el.innerHTML = ""
 
   for (let i = 0; i < Types.length; i++) {
     const li = document.createElement("div")
@@ -298,5 +296,4 @@ for (let i = 0; i < initTypes.length; i++) {
 }
 
 document.getElementById("btn_pause").addEventListener("click", handlePause)
-
 update()
